e00ecfc3abee382e71cd1703c414b607f9261a00
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-05 18:57:37 +0530

    Add implementation for checking prime numbers

---
+++ Basic_Maths/primeNumber.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  isPrime(int n){
+    if(n<=1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    for(int i =2; i<=sqrt(n); i++){ // brute force approach 
+        if(n%i ==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+// time complexity is O(sqrt(n))
+int main(){
+    int n;
+    cout <<"Enter a number n:";
+    cin>> n;
+    if(isPrime(n)){
+        cout<< n << " is a prime number";
+    }
+    else{
+        cout<< n << " is not a prime number";
+    }
+}
