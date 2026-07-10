544f023eb71fe2110f896d228da751d210cc5256
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-10 21:37:55 +0530

    Refactor rotate function to improve clarity and optimize space complexity

--- arrays/leftRotatebyn.cpp
+++ arrays/leftRotatebyn.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void roatate(int arr[], int n , int d){
+
     d = d%n;
 
     int temp[d];
@@ -21,6 +22,13 @@ void roatate(int arr[], int n , int d){
 
 }
 
+void rotate(int arr[], int n , int d){ // this is the optimal approach which take space complexity O(1)
+    reverse(arr, arr+d);
+
+    reverse(arr+d, arr+n);
+    reverse(arr,arr+n);
+}
+
 int main(){
      int n;
     cout<< "Enter the number of elements for the array";
@@ -31,10 +39,15 @@ int main(){
         cin >> arr[i];
     
 }
-roatate(arr,n,4);
-for(int i = 0; i<n; i++){
+// roatate(arr,n,4);
+// for(int i = 0; i<n; i++){
+//         cout<< arr[i];
+//     }
+    rotate(arr,n,5);
+    for(int i = 0; i<n; i++){
         cout<< arr[i];
     }
+
 
 return 0;
 }
