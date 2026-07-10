80b3412bbaa0c4dadfeba4523aa712b6b5c3fa15
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-10 19:15:19 +0530

    Add implementation of function to rotate an array by d elements

---
+++ arrays/leftRotatebyn.cpp
@@ -0,0 +1,43 @@
+//brute force approach 
+#include<bits/stdc++.h>
+using namespace std;
+
+void roatate(int arr[], int n , int d){
+    d = d%n;
+
+    int temp[d];
+    for(int i = 0; i<d; i++){
+        temp[i] = arr[i]; // store everything in temp 
+
+    }
+    for(int i = d; i<n; i++){
+        arr[i-d] = arr[i];
+
+    }
+    
+    for(int i = n-d; i<n; i++){
+        arr[i] = temp[i-(n-d)];
+    }
+
+}
+
+int main(){
+     int n;
+    cout<< "Enter the number of elements for the array";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    
+}
+roatate(arr,n,4);
+for(int i = 0; i<n; i++){
+        cout<< arr[i];
+    }
+
+return 0;
+}
+// time complexity is O(N)
+// space complexity is O(d)
+
