6391cc2164bf1c27a46a83dc00720069d0dc9c6b
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-12 17:43:56 +0530

    Add implementations of summissing and xormissing functions to find missing number in an array

--- arrays/missingNum.cpp
+++ arrays/missingNum.cpp
@@ -41,6 +41,31 @@ int hashmissing(vector<int>& arr){
     }
     return -1;
 } //time complexity = O(n) space complexity = O(n)
+int summissing(vector<int>& arr){
+    int n = arr.size()+1;
+    int sum = 0;
+
+    for(int i=0; i<n-1;i++){
+        sum +=arr[i];
+    }
+    long long totalSum = (n*(n+1))/2;
+
+    return totalSum - sum;
+} //time complexity = O(n) space complexity = O(1)
+
+int xormissing(vector <int>& arr){
+    int n = arr.size()+1;
+    int xor1 =0, xor2 = 0;
+    for(int i =0; i<n-1; i++){
+        xor2 = xor2^arr[i];
+        xor1 = xor1^(i+1);
+    }
+    xor1 = xor1^n;
+    return xor1^xor2;
+
+} // space complexity = O(1) time complexity = O(n)
+// but it is better then sum because while calculating sum we take long long which takes  more space than integer 
+
 int main(){
     int n ;
     cout << "Enter the size of the array";
@@ -49,8 +74,10 @@ int main(){
     cout << "Enter the elements of array: ";
     for(int i=0; i<n; i++){
         cin >> arr1[i];
-    };
+    }; 
     cout << missing(arr1)<<endl;
     cout <<hashmissing(arr1)<<endl ;
+    cout<< summissing(arr1)<<endl;
+    cout << xormissing(arr1);
 } 
 
