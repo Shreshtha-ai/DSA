e71d3664453930cbbce18021f23eb42dfa9a9565
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-12 17:14:01 +0530

    Add implementation of functions to find missing number in an array

---
+++ arrays/missingNum.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing(vector<int>& arr){
+
+    int n = arr.size()+1;
+
+    for(int i =1; i<=n;i++){
+        bool found = false;
+        for(int j =0; j<n-1; j++){
+            if(arr[j]==i){
+                found = true;
+                break;
+            }
+        }
+
+        if(!found)
+        return i;
+
+
+
+
+    }
+    return -1;
+}// time complexity = O(N^2) SPACE COMPLEXITY = O(1)
+ 
+
+int hashmissing(vector<int>& arr){
+    int n = arr.size()+1;
+
+    vector<int> hash(n+1,0);
+
+    for(int i =0; i<n-1; i++){
+        hash[arr[i]]++;
+
+    }
+    for(int i =1; i<=n; i++){
+        if(hash[i] ==0){
+            return i;
+        }
+    }
+    return -1;
+} //time complexity = O(n) space complexity = O(n)
+int main(){
+    int n ;
+    cout << "Enter the size of the array";
+    cin>> n;
+     vector<int> arr1(n);
+    cout << "Enter the elements of array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr1[i];
+    };
+    cout << missing(arr1)<<endl;
+    cout <<hashmissing(arr1)<<endl ;
+} 
+
