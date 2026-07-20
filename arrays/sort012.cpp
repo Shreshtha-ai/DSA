dc2caf2279142575ec0a302399151a4a296961e8
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-20 17:42:05 +0530

    Refactor sort012 function to use Dutch National Flag algorithm for improved performance

--- arrays/sort012.cpp
+++ arrays/sort012.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sort012(vector<int>& nums){
+void sort012(vector<int>& nums){ // this is not the optimal solution time complexity =  O(2N)
     int count0 = 0, count1 = 0, count2 = 0 ;
             for(int i = 0; i < nums.size(); i++) {
             if(nums[i] == 0) count0++;
@@ -26,6 +26,28 @@ void sort012(vector<int>& nums){
         }
 
 }
+
+
+void sortArr(vector<int>& nums){ // By Dutch flag algorithm 
+    int low  = 0, mid = 0, high = nums.size() -1;
+    while(mid<=high){
+        if(nums[mid] ==0){
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+
+        }
+        else if (nums[mid] == 1) {
+                mid++;
+        }
+        else {
+            swap(nums[mid], nums[high]);
+            high--;
+
+        }
+
+    }
+}
 int main(){
     int n;
     cout << "Enter the size of array: ";
@@ -35,7 +57,7 @@ int main(){
     for(int i=0; i<n; i++){
         cin >> arr[i];
 }
-sort012(arr);
+sortArr(arr);
     for(int x = 0; x < n; x++){
     cout<<arr[x];
     }
