5e013fad3727b167c97ed26bc8ddc5fb6b89a4b3
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-27 23:24:53 +0530

    Replace maxSubArray function call with maxSubArray2 for improved performance

--- arrays/subarraySum.cpp
+++ arrays/subarraySum.cpp
@@ -20,6 +20,24 @@ using namespace std;
         
     }
 
+    int maxSubArray2(vector<int>& nums) { // TC = O(N^3) SC = O(1)
+        int maxi = INT_MIN;
+
+        for(int i =0; i<nums.size(); i++){ 
+            int sum = 0;
+
+            for(int j =i; j<nums.size(); j++){
+                    sum += nums[j];
+                
+                maxi = max(maxi,sum);
+            }
+        }
+
+        return maxi;
+
+        
+    }
+
     int main(){
         int n;
     cout << "Enter the size of array: ";
@@ -29,7 +47,7 @@ using namespace std;
     for(int i=0; i<n; i++){
         cin >> arr[i];
 }
-int a = maxSubArray(arr);
+int a = maxSubArray2(arr);
 cout << a;
     
 
