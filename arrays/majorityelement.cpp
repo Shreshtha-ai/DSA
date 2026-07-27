c68e55811d7ca4bfe28a2e4985b1e62149e66339
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-27 22:38:07 +0530

    Add implementation of majorityElementbyMVA function using Moore's Voting Algorithm for improved performance

--- arrays/majorityelement.cpp
+++ arrays/majorityelement.cpp
@@ -38,6 +38,40 @@ int majorityElementsHash(vector<int>& nums){
     return -1;
 }
 
+int majorityElementbyMVA(vector<int>& nums){  //TC = O(N)+ O(N) SC = O(1)
+    int cnt =0;
+    int el;
+    for(int i = 0; i<nums.size();i++){
+        if(cnt ==0){
+            cnt =1;
+            el = nums[i];
+        }
+        else if(nums[i] ==el){
+            cnt++;
+
+        }
+        else {
+            cnt --;
+
+        }
+
+        }
+
+        int cnt1 =0;
+        for(int i =0; i<nums.size();i++){
+            if(nums[i] == el){
+                cnt1++;
+            }
+
+        }
+        if(cnt1> (nums.size()/2)){
+            return el;
+        }
+        return -1;
+    
+    }
+
+
 int main(){
         int n;
     cout << "Enter the size of array: ";
@@ -47,7 +81,7 @@ int main(){
     for(int i=0; i<n; i++){
         cin >> arr[i];
 }
-int a = majorityElementsHash(arr);
+int a = majorityElementbyMVA(arr);
 cout << a;
     
 
