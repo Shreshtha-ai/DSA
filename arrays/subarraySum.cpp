c28d906bbd218b4d1dcc37b48f8403525dcbc2f5
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-07-27 23:00:59 +0530

    Add implementation of maxSubArray function to find the maximum subarray sum using a brute-force approach

---
+++ arrays/subarraySum.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    
+    int maxSubArray(vector<int>& nums) { // TC = O(N^3) SC = O(1)
+        int maxi = INT_MIN;
+
+        for(int i =0; i<nums.size(); i++){
+            for(int j =i; j<nums.size(); j++){
+                int sum = 0;
+                for(int k = i; k<=j; k++){
+                    sum += nums[k];
+                }
+                maxi = max(maxi,sum);
+            }
+        }
+
+        return maxi;
+
+        
+    }
+
+    int main(){
+        int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the elements of array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+}
+int a = maxSubArray(arr);
+cout << a;
+    
+
+
+}
