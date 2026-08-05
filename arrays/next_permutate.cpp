7cb0b5e465c328fdae3c1c776121f32037073381
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-08-05 11:43:38 +0530

    Refactor comments in next_permute function for improved readability

--- arrays/next_permutate.cpp
+++ arrays/next_permutate.cpp
@@ -24,6 +24,8 @@ vector<int> next_permute(vector<int> &nums){ //TC = O(3N) SC = O(1)
     reverse(nums.begin() + ind + 1, nums.end());
     return nums;
     // in this code we first find a number which is smaller than its next number from the right side of the array. 
-    // If we find such a number then we will find a number which is greater than this number from the right side of the array and swap them. After that we will reverse the numbers after the index of the first number we found. If we don't find any such number then we will reverse the whole array.
+    // If we find such a number then we will find a number which is greater than this number from the right side of the array and swap them. 
+    // After that we will reverse the numbers after the index of the first number we found. 
+    //If we don't find any such number then we will reverse the whole array.
 
 }
